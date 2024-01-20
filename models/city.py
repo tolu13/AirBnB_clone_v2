170952f6f4c389d691c3d2820a02bac586ed5d41
thugginnn <paul.iyiade4@gmail.com>
2024-01-20 13:22:32 -0800

    city.py

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for the city class"""
+"""This is the city class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
@@ -19,7 +19,3 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="cities")
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a new City."""
-        super().__init__(*args, **kwargs)
