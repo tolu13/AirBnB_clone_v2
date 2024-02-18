c654d31b7605523d53e31b8edc24caaef89e8abf
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-02-18 21:18:42 +0000

    ok

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -13,20 +13,20 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Returns the message 'Hello HBNB!'."""
-    return 'Hello HBNB!'
+    return ("Hello HBNB!")
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Returns the message 'HBNB'."""
-    return 'HBNB'
+    return ("HBNB")
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     """Returns 'C ' followed by the value of the text variable."""
     text = unquote(text.replace("_", " "))
-    return 'C {}'.format(text)
+    return ("C {}").format(text)
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
@@ -34,9 +34,9 @@ def c_route(text):
 def python_route(text):
     """Returns 'Python ' followed by the value of the text variable."""
     text = unquote(text.replace("_", " ")) if text else 'is cool'
-    return 'Python {}'.format(text)
+    return ("Python {}").format(text)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000  debug=None)
 
