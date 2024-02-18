39b76dbc2e68f1db0f136d2948fc68115f6764bb
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-02-18 22:38:13 +0000

    rh

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,41 +1,37 @@
 #!/usr/bin/python3
-
-"""
-This script starts a Flask web application with specific routes.
+""" Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
 """
 
 from flask import Flask
-from urllib.parse import unquote
 
 app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Returns the message 'Hello HBNB!'."""
-    return ("Hello HBNB!",)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Returns the message 'HBNB'."""
-    return ("HBNB",)
+    """Returns a given string"""
+    return ("HBNB")
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """Returns 'C ' followed by the value of the text variable."""
-    text = unquote(text.replace("_", " "))
-    return ("C {}".format(text),)
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
-    """Returns 'Python ' followed by the value of the text variable."""
-    text = unquote(text.replace("_", " ")) if text else 'is cool'
-    return ("Python {}".format(text),)
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
