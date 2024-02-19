a15a99b607f5e2d4ac04977e64ac5e6893eba4da
thugginnn <paul.iyiade4@gmail.com>
2024-02-19 02:09:21 -0800

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-""" AA script that starts a Flask web application
-listening on 0.0.0.0, port 5000 """
-
+"""A script that starts a Flask web application"""
 from flask import Flask
-from urllib.parse import unquote
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-app = Flask("__name__")
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """ this return a string"""
-    return ("Hello HBNB!")
+    """print web"""
+    return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route('/hbnb')
 def hbnb():
-    """ return a given string """
-    return ("HBNB")
+    """print web"""
+    return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """ return a given string """
-    text = unquote(text.replace("_", " "))
-    return ("C {}").format(text)
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """print C followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
